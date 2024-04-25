b46bf76eb96a86969401356b5e7ca3f389ff1c4d
Adarsh Gupta <adarshgupta.cse18@iitbhu.ac.in>
2024-04-26 02:54:03 +0530

    Publishing to queue with delay

--- services/postService.js
+++ services/postService.js
@@ -1,9 +1,13 @@
 const Post = require('../models/post');
+const queuePublisher = require('../queues/publisher');
 
 class PostService {
     async create(args) {
         // TODO: validate args
         const post = await new Post(args).create();
+        const { id, scheduled_at } = post;
+        queuePublisher.sendToQueue({ id, scheduled_at });
+        // TODO: handle if this queue publishing failed but post was created
         return post;
     }
 }
