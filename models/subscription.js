ba329b9044696c94f3d15ff2b280848ddaea2ac5
Adarsh Gupta <adarshgupta.cse18@iitbhu.ac.in>
2024-04-25 19:45:46 +0530

    Updating Subscription with constructor

--- models/subscription.js
+++ models/subscription.js
@@ -1,10 +1,15 @@
 const db = require('../db');
 
 class Subscription {
-    async create(userId, topicId) {
+    constructor(userId, topicId) {
+        self.userId = userId;
+        self.topicId = topicId;
+    }
+    
+    async create() {
         try {
             const statement = 'INSERT INTO subscriptions(user_id, topic_id) VALUES($1, $2)';
-            const values = [userId, topicId];
+            const values = [self.userId, self.topicId];
             const result = await db.query(statement, values);
             if (result.rows.length > 0) {
                 return result.rows[0];
--- services/subscriptionService.js
+++ services/subscriptionService.js
@@ -2,7 +2,7 @@ const Subscription = require('../models/subscription');
 
 class SubscriptionService {
     async subscribe(userId, topicId) {
-        const subscription = await Subscription.create(userId, topicId);
+        const subscription = await Subscription(userId, topicId).create();
         return subscription;
     }
 }
